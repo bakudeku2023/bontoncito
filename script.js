0b2b941e526d40e7f441ef53e8ab94e7e533bf62
bakudeku2023 <136014279+bakudeku2023@users.noreply.github.com>
2023-07-04 15:43:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function escribimensaje(){
+
+    if (confirm("¿te gusta la coca?")) {
+        alert("que bueno ahora dame comida que tengo hambre :D")
+      
+        
+    }
+    
+    else{
+        alert("andate no tas vienvenido >:( ")
+    }
+}
